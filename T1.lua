1b4ad5a296c3d2386e6fdcaee2a7e416d943cfdc
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2024-12-13 01:38:52 +0100

    Update T1.lua

--- T1.lua
+++ T1.lua
@@ -1,2 +1,2 @@
-os.run("wget https://raw.githubusercontent.com/sebastian2007bro/ComputerCraftPrograms/refs/heads/main/TrainChecker.lua?token=GHSAT0AAAAAAC2IUPA5ACY73AK67T2ORJKYZ23QDKQ /TrainChecker.lua")
-os.run("wget https://raw.githubusercontent.com/sebastian2007bro/ComputerCraftPrograms/refs/heads/main/Rail0001_.txt?token=GHSAT0AAAAAAC2IUPA52GT4M5VRTCT5H6KOZ23QEFA /Rail0001_.txt")
+os.execute("wget https://raw.githubusercontent.com/sebastian2007bro/ComputerCraftPrograms/refs/heads/main/TrainChecker.lua?token=GHSAT0AAAAAAC2IUPA5ACY73AK67T2ORJKYZ23QDKQ /TrainChecker.lua")
+os.execute("wget https://raw.githubusercontent.com/sebastian2007bro/ComputerCraftPrograms/refs/heads/main/Rail0001_.txt?token=GHSAT0AAAAAAC2IUPA52GT4M5VRTCT5H6KOZ23QEFA /Rail0001_.txt")
